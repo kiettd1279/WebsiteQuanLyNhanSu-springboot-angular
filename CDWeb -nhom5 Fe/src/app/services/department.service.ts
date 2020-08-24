5aad468ff415b74568ded80264956932de504fe1
Hong <16130394@st.hcmuaf.edu.vn>
2020-08-24 22:30:08 +0700

    update_department

--- CDWeb -nhom5 Fe/src/app/services/department.service.ts
+++ CDWeb -nhom5 Fe/src/app/services/department.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Department } from '../models/department';
 import { Observable } from 'rxjs';
-import { Paging } from '../models/paging';
-import { RootObj } from '../models/root-obj';
 import { ApiService } from './api.service';
+import { RootObj } from '../models/root-obj';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +14,19 @@ export class DepartmentService {
 
   list(): Observable<[Department]> {
    
-    console.log(`${this.apiService.apiUrl.departments.home}`);   
+    // console.log(`${this.apiService.apiUrl.departments.home}`);   
     return this.apiService.get<[Department]>
       (`${this.apiService.apiUrl.departments.home}`);
   }
+  save(data: Department): Observable<RootObj<Department>> {
+    if (!data.id) {
+      return this.apiService.post<RootObj<Department>>(this.apiService.apiUrl.departments.home, data);
+    } else {
+      return this.apiService.put<RootObj<Department>>(`${this.apiService.apiUrl.departments.home}/${data.id}`, data);
+    }
+  }
+  // getDeptById(id: Number): Observable<RootObj<Department>> {
+  //   return this.apiService.get<RootObj<Department>>
+  //     (`${this.apiService.apiUrl.departments.getDept}/${id}`, id);
+  // }
 }
